66b1614619137ff8e005b963d2c015dc6fa5fbda
ARAMALI Mohammed <mouhammedaramali@gmail.com>
2024-05-11 21:33:26 +0100

    project setup

---
+++ HealthAi/src/main/java/fsts/ma/HealthAi/HealthAiApplication.java
@@ -0,0 +1,13 @@
+package fsts.ma.HealthAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAiApplication.class, args);
+	}
+
+}
